531c91e881d6f3da00aaca86c564c331d7bcae11
MAY14\May 1 <huynhdieulac@gmail.com>
2022-08-13 11:44:36 +0700

    Changed Update Part 1

---
+++ src/main/java/com/example/storeonline/StoreonlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreonlineApplication.class, args);
+	}
+
+}
